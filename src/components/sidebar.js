4b665a7e678e8308092fbf9db304cc66bf3e53ba
Leo <leonardogds015@gmail.com>
2022-07-01 17:11:32 -0300

    Adicionando icones Sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.css';
+import { BsPersonFill, BsPersonPlusFill, BsCardHeading, BsBoxArrowRight, BsHouseDoorFill, BsPersonCircle, BsPencilSquare } from 'react-icons/bs';
 
 
 export default function Sidebar() {
@@ -12,14 +13,14 @@ export default function Sidebar() {
 
                 <li class="nav-item">
                     <a class="nav-link " href="/Home">
-                        <i class="bi bi-grid"></i>
+                        <i class="bi bi-grid"><BsHouseDoorFill/></i>
                         <span>Home</span>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="/Profile">
-                        <i class="bi bi-person"></i>
+                        <i class="bi bi-person"><BsPersonCircle/></i>
                         <span>Profile</span>
                     </a>
                 </li>
@@ -29,13 +30,13 @@ export default function Sidebar() {
 
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="#">
-                        <i class="bi bi-person"></i>
+                        <i class="bi bi-person"><BsPersonFill/></i>
                         <span>Users</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="/NewUser">
-                        <i class="bi bi-person"></i>
+                        <i class="bi bi-person"><BsPersonPlusFill/></i>
                         <span>New User</span>
                     </a>
                 </li>
@@ -43,22 +44,23 @@ export default function Sidebar() {
                 <li class="nav-heading">Cards</li>     
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="pages-blank.html">
-                        <i class="bi bi-file-earmark"></i>
+                        <i class="bi bi-file-earmark"><BsCardHeading/></i>
                         <span>Card List</span>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="/NewCard">
-                        <i class="bi bi-file-earmark"></i>
+                        <i class="bi bi-file-earmark"><BsPencilSquare/></i>
                         <span>New Card</span>
                     </a>
                 </li>
 
-                <li class="nav-heading"></li> 
+                <li class="nav-heading"></li>
+                 
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="pages-blank.html">
-                        <i class="bi bi-file-earmark"></i>
+                        <i class="bi bi-file-earmark"><BsBoxArrowRight/></i>
                         <span>Sign Out</span>
                     </a>
                 </li>
